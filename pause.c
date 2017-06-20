da1dd91311e5f316cc1ae869ac0689124210fd58
goldbeef <goldbeef@163.com>
2017-06-20 19:41:04 +0800

    update

---
+++ pause.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: pause.c
+    > Author: goldbeef
+    > Mail: goldbeef@163.com 
+    > Created Time: 2017年06月20日 星期二 19时34分32秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pause();
+    return 0;
+}
